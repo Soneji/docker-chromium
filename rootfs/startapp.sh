a0b249acece476a41e985ddb7a3735b5adaf8b8d
overclockedllama <overclockedllama@javamail.co.uk>
2020-05-18 19:59:36 +0100

    Update startapp.sh

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,4 +6,4 @@ set -u # Treat unset variables as an error.
 export HOME=/config
 mkdir -p /config/profile
 whoami
-exec /usr/bin/firefox_wrapper --no-sandbox --user-data-dir=/config/profile >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+exec /usr/bin/firefox_wrapper --no-sandbox --user-data-dir=/config/profile >> /config/log/chromium/output.log 2>> /config/log/chromium/error.log
